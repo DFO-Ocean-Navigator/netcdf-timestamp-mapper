ab6773d779ee2b879d6e9bfcf0e712dbc5816b92
Nabil M <nmiri127@gmail.com>
2019-06-27 16:05:17 -0230

    Add generic timer function

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 all: src/main.cpp
 	make clean
 	mkdir -p ./build
-	g++-8 -o build/nc-timestamp-mapper src/Database.cpp src/main.cpp -std=c++17 -lstdc++fs -lnetcdf_c++4 -lsqlite3 -I./src/ThirdParty/ -pedantic -W -Wall -Wextra -O
+	g++-8 -o build/nc-timestamp-mapper src/Database.cpp src/main.cpp -std=c++17 -lstdc++fs -lnetcdf_c++4 -lsqlite3 -I./src/ThirdParty/ -pedantic -W -Wall -Wextra -O3
 
 tests: tests/main.cpp
 	mkdir -p ./build
---
+++ src/Timer.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <type_traits>
+#include <utility>
+#include <chrono>
+
+namespace Utils {
+
+/// https://stackoverflow.com/a/24469673/2231969
+template <class Func, class... Args>
+inline auto timer(Func func, Args&& ... args) -> typename std::enable_if_t<
+    !std::is_same_v<decltype( func( std::forward<Args>(args)... ) ),
+    void>,
+    std::pair<double, decltype( func( std::forward<Args>(args)... ) ) >
+    > {
+
+    static_assert(!std::is_void_v<decltype( func(args...) )>,
+                  "Call timer_void if return type is void!");
+
+    const auto& start{ std::chrono::high_resolution_clock::now() };
+    auto funcRetVal{ func(args...) };
+    const auto& end{ std::chrono::high_resolution_clock::now() };
+
+    const std::chrono::duration<double, std::milli> elapsed{ end - start };
+
+    return { elapsed.count(), funcRetVal };
+}
+
+template <class Func, class... Args>
+inline auto timer(Func func, Args&& ... args) -> typename std::enable_if_t<
+    std::is_same_v<decltype( func( std::forward<Args>(args)... ) ),
+    void>
+    > {
+
+    static_assert(std::is_void_v<decltype( func(args...) )>,
+                  "Call timer for non void return type");
+
+    const auto& start{ std::chrono::high_resolution_clock::now() };
+    func(args...);
+    const auto& end { std::chrono::high_resolution_clock::now() };
+
+    const std::chrono::duration<double, std::milli> elapsed{ end - start };
+
+    return elapsed.count();
+}
+
+} // namespace Utils
