b73d416cf8de8e77f3c14c8e66a0ce81734a3cf8
Nabil M <nmiri127@gmail.com>
2019-07-17 15:58:12 -0230

    Fix another CLI bug
    * cxxopts is a giant pain.

--- src/CLIOptions.hpp
+++ src/CLIOptions.hpp
@@ -19,12 +19,12 @@ namespace tsm::cli {
         ("i,input-dir", "Input directory.", cxxopts::value<std::string>())
         ("n,dataset-name", "Dataset name (no spaces).", cxxopts::value<std::string>())
         ("o,output-dir", "Output directory.", cxxopts::value<std::string>())
-        ("regen-indices", "Regenerate indices.", cxxopts::value<bool>()->implicit_value("false"))
-        ("f,forecast", "Forecast dataset type.", cxxopts::value<bool>()->implicit_value("false"))
-        ("h,historical", "Historical dataset type.", cxxopts::value<bool>()->implicit_value("false"))
-        ("variables-combined", "Dataset variables are all in one file.", cxxopts::value<bool>()->implicit_value("false"))
-        ("variables-split", "Dataset variables are split across multiple files.", cxxopts::value<bool>()->implicit_value("false"))
-        ("h,help", "Print help.")
+        ("regen-indices", "Regenerate indices.", cxxopts::value<bool>())
+        ("f,forecast", "Forecast dataset type.", cxxopts::value<bool>())
+        ("h,historical", "Historical dataset type.", cxxopts::value<bool>())
+        ("c,variables-combined", "Dataset variables are all in one file.", cxxopts::value<bool>())
+        ("s,variables-split", "Dataset variables are split across multiple files.", cxxopts::value<bool>())
+        ("help", "Print help.")
         ;
 
         const auto& result{ options.parse(argc, argv) };
@@ -53,11 +53,11 @@ struct [[nodiscard]] CLIOptions {
     explicit CLIOptions(const cxxopts::ParseResult& result) :   InputDir{ result["input-dir"].as<std::string>() },
                                                                 DatasetName{ result["dataset-name"].as<std::string>() },
                                                                 OutputDir{ result["output-dir"].as<std::string>() },
-                                                                RegenIndices{ result["regen-indices"].as<bool>() },
-                                                                Forecast{ result["forecast"].as<bool>() },
-                                                                Historical{ result["historical"].as<bool>() },
-                                                                VarsCombined{ result["variables-combined"].as<bool>() },
-                                                                VarsSplit{ result["variables-split"].as<bool>() } {}
+                                                                RegenIndices{ result.count("regen-indices") > 0 },
+                                                                Forecast{ result.count("forecast") > 0 },
+                                                                Historical{ result.count("historical") > 0 },
+                                                                VarsCombined{ result.count("variables-combined") > 0 },
+                                                                VarsSplit{ result.count("variables-split") > 0 } {}
 
     const std::string InputDir;
     const std::string DatasetName;
